75a7634574ed3defc7fc28fc94853893ea4b8acc
gouthamsundaram100 <49850003+gouthamsundaram100@users.noreply.github.com>
2020-11-24 15:01:57 -0500

    testing upload functionality

--- CustomWidget/excel.js
+++ CustomWidget/excel.js
@@ -344,28 +344,31 @@
                                 var rec_count = 0;
 
                                 var len = 0;
-                                if (lengthfield === 9) {
+                                if (lengthfield === 4) {
                                     for (var i = 1; i < result.split("[$@~!~@$]").length; i++) {
                                         if (result.split("[$@~!~@$]")[i].length > 0) {
 
                                             var rec = result.split("[$@~!~@$]")[i].split("[#@~!~@#]");
                                             if (rec.length > 0) {
-                                                len = rec[0].trim().length + rec[1].trim().length + rec[2].trim().length + rec[3].trim().length + rec[4].trim().length + rec[
-                                                    5].trim().length + rec[6].trim().length + rec[7].trim().length + rec[8].trim().length;
+                                                len = rec[0].trim().length + rec[1].trim().length + rec[2].trim().length + rec[3].trim().length;// + rec[4].trim().length + rec[
+                                                    //5].trim().length + rec[6].trim().length + rec[7].trim().length + rec[8].trim().length;
                                                 if (len > 0) {
                                                     rec_count = rec_count + 1;
                                                     result_final.push({
                                                         'ID': i,
-                                                        'DATE': rec[0].trim(),
-                                                        'COUNTRY_CODE': rec[1].trim(),
-                                                        'COMPANY_CODE': rec[2].trim(),
-                                                        'TYPE': rec[3].trim(),
-                                                        'VALUE_DATE': rec[4].trim(),
-                                                        'AMOUNT': rec[5].trim().replace(/[,]/g, ""),
-                                                        'CURRENCY': rec[6].trim(),
-                                                        'COMMENTS': rec[7].trim().replace(/["'\n\r]/g, ""),
-                                                        'LOCK_FLAG': rec[8].trim(),
+                                                        'Account': rec[0].trim(),
+                                                        'Product': rec[1].trim(),
+                                                        'Date': rec[2].trim(),
+                                                        'Number': rec[3].trim(),
+                                                        //'VALUE_DATE': rec[4].trim(),
+                                                        //'AMOUNT': rec[5].trim().replace(/[,]/g, ""),
+                                                        //'CURRENCY': rec[6].trim(),
+                                                        //'COMMENTS': rec[7].trim().replace(/["'\n\r]/g, ""),
+                                                        //'LOCK_FLAG': rec[8].trim(),
                                                     });
+                                                    //materialMember=({id:ID})
+                                                    PlanningModel_1.createMembers("Product", {id: rec[2].trim(), description: ""});
+
                                                 }
                                             }
                                         }
@@ -401,7 +404,7 @@
                                         var oModel = new JSONModel();
 
                                         console.log(result_final);
-                                        oModel.loadData("processData.xsjs", JSON.stringify(result_final), true, 'POST', false, true, oHeaders);
+                                        //oModel.loadData("processData.xsjs", JSON.stringify(result_final), true, 'POST', false, true, oHeaders);
 
                                         oModel.attachRequestCompleted(function() {
                                             var result = oModel.getData();
