31e0b8ad743a986598130723d10be979d482bc5f
gouthamsundaram100 <49850003+gouthamsundaram100@users.noreply.github.com>
2020-12-11 18:07:30 -0500

    Update excel.js

--- CustomWidget/excel.js
+++ CustomWidget/excel.js
@@ -275,7 +275,7 @@
 
     	  sap.ui.getCore().attachInit(function() {
     	    "use strict";
-    	    jQuery.sap.registerModulePath("myView", "https://gouthamsundaram100.github.io/SAP/");
+    	    jQuery.sap.registerModulePath("myView.Template", "./");
     	    //jQuery.sap.require('myView.gouthamsundaram100CustomWidget.excel');
     	    //sap.ui.localResources("myView.Template");
     	    //sap.ui.localResources("myView");
