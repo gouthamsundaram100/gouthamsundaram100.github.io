9de4752fc85f0a043b6f6f886db995508da2796d
gouthamsundaram100 <49850003+gouthamsundaram100@users.noreply.github.com>
2020-12-22 16:25:53 -0500

    Update excel.js

--- CustomWidget/excel.js
+++ CustomWidget/excel.js
@@ -274,7 +274,7 @@
 
     	  sap.ui.getCore().attachInit(function() {
     	    "use strict";
-    	    jQuery.sap.registerModulePath("myView.Template","https://gouthamsundaram100.github.io/CustomWidget/excel.js");
+    	    jQuery.sap.registerModulePath("myView","https://gouthamsundaram100.github.io/CustomWidget/excel.js");
     	    //### Controller ###
     	    sap.ui.require([
     	      "jquery.sap.global",
