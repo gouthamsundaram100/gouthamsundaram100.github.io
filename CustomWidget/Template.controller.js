d4a6bb24d9279ade0e0efac79deea454f314e4a8
gouthamsundaram100 <49850003+gouthamsundaram100@users.noreply.github.com>
2020-12-29 12:39:11 -0500

    Update Template.controller.js

--- CustomWidget/Template.controller.js
+++ CustomWidget/Template.controller.js
@@ -1,4 +1,42 @@
-	sap.ui.define([
+var that=this;
+var that_ = that;
+
+    	  widgetName = changedProperties.widgetName;
+    	  if (typeof widgetName === "undefined") {
+    	    widgetName = that._export_settings.title.split("|")[0];
+    	  }
+
+
+    	  div = document.createElement('div');
+    	  div.slot = "content_" + widgetName;
+
+    	  if (that._firstConnection === 0) {
+    	    let div0 = document.createElement('div');
+    	    div0.innerHTML = '<?xml version="1.0"?><script id="oView_' + widgetName + '" name="oView_' + widgetName + '" type="sapui5/xmlview"><mvc:View height="100%" xmlns="sap.m" xmlns:u="sap.ui.unified" xmlns:f="sap.ui.layout.form" xmlns:core="sap.ui.core" xmlns:mvc="sap.ui.core.mvc" controllerName="CustomWidget.Template"><f:SimpleForm editable="true"><f:content><Label text="Upload"></Label><VBox><u:FileUploader id="idfileUploader" width="100%" useMultipart="false" sendXHR="true" sameFilenameAllowed="true" buttonText="" fileType="XLSM" placeholder="" style="Emphasized" change="onValidate"></u:FileUploader></VBox></f:content></f:SimpleForm></mvc:View></script>';
+    	    _shadowRoot.appendChild(div0);
+    	    // https://sapui5.hana.ondemand.com/1.60.29/resources/myView/Template.controller.js
+    	    let div1 = document.createElement('div');
+    	    div1.innerHTML = '<?xml version="1.0"?><script id="myXMLFragment_' + widgetName + '" type="sapui5/fragment"><core:FragmentDefinition xmlns="sap.m" xmlns:core="sap.ui.core"><SelectDialog title="Partner Number" class="sapUiPopupWithPadding"  items="{' + widgetName + '>/}" search="_handleValueHelpSearch"  confirm="_handleValueHelpClose"  cancel="_handleValueHelpClose"  multiSelect="true" showClearButton="true" rememberSelections="true"><StandardListItem icon="{' + widgetName + '>ProductPicUrl}" iconDensityAware="false" iconInset="false" title="{' + widgetName + '>partner}" description="{' + widgetName + '>partner}" /></SelectDialog></core:FragmentDefinition></script>';
+    	    _shadowRoot.appendChild(div1);
+
+    	    let div2 = document.createElement('div');
+    	    div2.innerHTML = '<div id="ui5_content_' + widgetName + '" name="ui5_content_' + widgetName + '"><slot name="content_' + widgetName + '"></slot></div>';
+    	    _shadowRoot.appendChild(div2);
+
+    	    that_.appendChild(div);
+
+    	    var mapcanvas_divstr = _shadowRoot.getElementById('oView_' + widgetName);
+    	    var mapcanvas_fragment_divstr = _shadowRoot.getElementById('myXMLFragment_' + widgetName);
+
+    	    Ar.push({
+    	      'id': widgetName,
+    	      'div': mapcanvas_divstr,
+    	      'divf': mapcanvas_fragment_divstr
+    	    });
+    	  }
+
+    	  that_._renderExportButton();	
+sap.ui.define([
     	      "jquery.sap.global",
     	      "sap/ui/core/mvc/Controller",
     	      "sap/ui/model/json/JSONModel",
@@ -18,7 +56,7 @@
     	      'sap/m/BusyDialog'
     	    ], function(jQuery, Controller, JSONModel, MessageToast, coreLibrary, Core, Filter, mobileLibrary, MessageBox, DateRange, DateFormat, BindingMode, Fragment, Token, FilterOperator, ODataModel, BusyDialog) {
     	      "use strict";
-    	      var that=this;
+    	      
     	      var busyDialog = (busyDialog) ? busyDialog : new BusyDialog({});
 
     	      return Controller.extend("CustomWidget.Template", {
