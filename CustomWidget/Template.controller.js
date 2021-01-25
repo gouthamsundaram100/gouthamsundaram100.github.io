3a3f419f7258754aeaac31cd364ad424abd2048c
gouthamsundaram100 <49850003+gouthamsundaram100@users.noreply.github.com>
2021-01-25 16:00:30 -0500

    Update Template.controller.js

--- CustomWidget/Template.controller.js
+++ CustomWidget/Template.controller.js
@@ -286,7 +286,7 @@
     	            that._firstConnection = 1;
     	          }
     	        },
-    	        console.log(that._firstConnection);
+    	        console.log((that._firstConnection).toString());
     	        console.log("Outside of onValidate");
     	        onValidate: function(e) {
     	        console.log("In onValidate Function");
